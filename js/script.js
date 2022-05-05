3a70ce65f5d8fe9e8a1745fa3ce9d9de524db06b
PaoloGit <paolo.sergi.88@gmail.com>
2022-05-05 16:08:03 +0200

    aggiunto titolo

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let myRow = document.querySelector(".row");
 
 for (let i=1; i<=100; i++){
-    myRow.innerHTML += `<div class="col m-3 number${i}"></div>`;
+    myRow.innerHTML += `<div class="col-auto m-3 number${i}"></div>`;
     let myCol = document.querySelector(`.number${i}`);
     myCol.innerHTML = `<article class="dom">${i}</article>`;
 
